614e7f8d0972035c17bbf50e1b313af0060cbe80
Mariano Gobea Alcoba <gobeamariano@gmail.com>
2023-04-21 15:01:13 -0300

    :sparkles: With fragments created

--- app/src/main/java/com/example/pokemoskotlin/ListFragment.kt
+++ app/src/main/java/com/example/pokemoskotlin/ListFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.pokemoskotlin.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
     override fun onCreateView(
@@ -12,6 +15,16 @@ class ListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        val view = FragmentListBinding.inflate(inflater)
+
+        val recycler = view.pokemonRecycler
+
+        // Para el layout manager en la activity pasabamos this como contexto. Pero el fragment no tiene contexto por si solo.
+        // Debemos pedirle el contexto a la activity
+        recycler.layoutManager = LinearLayoutManager(requireActivity())
+
+        // Challenge: Crear el adaptar para nuestro recycler!!!
+
+        return view.root
     }
 }
