c229552b0280d3e8aeb978abea865d91052a120c
Mariano Gobea Alcoba <gobeamariano@gmail.com>
2023-05-01 13:27:22 -0300

    :recycle: With database SQLite built

--- app/src/main/java/com/example/pokemoskotlin/Pokemon.kt
+++ app/src/main/java/com/example/pokemoskotlin/Pokemon.kt
@@ -1,11 +1,22 @@
 package com.example.pokemoskotlin
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
+@Entity("pokemones")
 @Parcelize
 data class Pokemon (
-    val id: Int, val name: String, val hp: Int, val attack: Int, val defense: Int,
-    val specialAttack: Int, val specialDefense: Int,
-    val speed: Int, val type: String, val imageUrl: String, val soundId: Int
+    @PrimaryKey val id: Int,
+    val name: String,
+    val hp: Int,
+    val attack: Int,
+    val defense: Int,
+    val specialAttack: Int,
+    val specialDefense: Int,
+    val speed: Int,
+    val type: String,
+    val imageUrl: String,
+    val soundId: Int
 ): Parcelable
